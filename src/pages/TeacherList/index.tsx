b3458e67775b24f916a735d35c71abe5ba257bb5
José Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-10-10 19:40:35 -0300

    :sparkles: tab navigation has been finished

---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import styles from './styles';
+
+function TeacherList() { // eslint-disable-line
+  return <View style={styles.container} />;
+}
+
+export default TeacherList;
